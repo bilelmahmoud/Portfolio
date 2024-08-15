ed2d4fadf776526bfb59e09efb0e51550d5b95c0
bilelmahmoud <bilelbenmahmoud777@gmail.com>
2024-08-14 23:49:00 -0400

    Commencer la section Hero en intégrant l'image, les icônes et le titre

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className='container'>
 
       <Header />
-      <div className='divider' />
+    
       <Hero />
       <div className='divider' />
       <Main />
--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -4,9 +4,28 @@ import './hero.css'
 const Hero = () => {
 
   return (
-    <>
-      hero
-    </>
+   
+      <section className='hero flex'>
+         <div className='left-section '>
+           <div className='parent-avatar flex'>
+              <img src="./avatar-bilel.jpg" className='avatar' alt="" />
+              <div className='icon-verified'></div>
+           </div>
+           <h1 className='title'>Développeur Full Stack</h1>
+           <p className='sub-title'> Passionné de solutions web innovantes, je suis un développeur Full Stack spécialisé dans la création d'expériences
+           utilisateur exceptionnelles. Mon expertise couvre la conception et la mise en œuvre d'interfaces attrayantes et fonctionnelles</p>
+           <div className='icons flex'>
+              <div className='icon icon-linkedin'></div>
+              <div className='icon icon-github'></div>
+           </div>
+         </div>
+
+
+         <div className='right-section annimation border'>
+            annimation
+         </div>
+      </section>
+   
   )
 }
 
