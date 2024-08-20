66606348887f95f2a913a6a28f80809db164f438
bilelmahmoud <bilelbenmahmoud777@gmail.com>
2024-08-20 16:57:40 -0400

    Concevoir le design de la carte pour la section projet

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -13,10 +13,38 @@ const Main = () => {
                   <button>PHP</button>
           </section>
 
-          <section className=' border right-section'>
-              <article className='card'>
+          <section className='right-section'>
+
+
+          {["aa", "bb", "cc"].map((item) => {
+
+            return(
+              <article key={item}  className='card'>
+                    <img width={266} src="./htmlcssjs-details.png" alt="" />
+                    <div style={{width:"266px"}} className="box">
+                          <h1 className="title">landing page 2</h1>
+                          <p className="sub-title">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed! Quidem, fugit quia maxime natus illum soluta quo voluptatibus laudantium consequuntur sapiente voluptates atque amet pariatur repudiandae quaerat, quod ipsa!
+                          </p>
+                          <div  className="flex icons">
+                              <div style={{gap : "11px"}} className='flex'>
+                                <div className="icon-link"></div>
+                                <div className="icon-github"></div>
+                              </div>
+                              <a className='link flex' href="">
+                                more 
+                                <span style={{alignSelf: "end"}} className='icon-arrow-right'>
+
+                                </span>
+                              </a>
+                          </div>
+                    </div>
 
               </article>
+            )
+
+          })}
+              
           </section>
     </main>
   )
