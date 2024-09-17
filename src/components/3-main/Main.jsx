7cf9199936c41218364b6741f4e012e1dfcb12a3
bilelmahmoud <bilelbenmahmoud777@gmail.com>
2024-09-16 22:34:35 -0400

    Ajouter une animation de filtre avec Framer Motion

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './main.css'
 import { myProjects } from './MyProjects';
+import { AnimatePresence, motion } from 'framer-motion';
 
 
 
@@ -80,37 +81,43 @@ const Main = () => {
           </section>
 
           <section className=' flex right-section'>
+          <AnimatePresence>
 
 
-          {arr.map((item) => {
+              {arr.map((item) => {
 
-            return(
-              <article key={item.imgpath}  className='card'>
-                    <img width={266} src={item.imgpath} alt="" />
-                    <div style={{width:"266px"}} className="box">
-                          <h1 className="title">{item.projectTitle}</h1>
-                          <p className="sub-title">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed! Quidem, fugit quia maxime natus illum soluta quo voluptatibus laudantium consequuntur sapiente voluptates atque amet pariatur repudiandae quaerat, quod ipsa!
-                          </p>
-                          <div  className="flex icons">
-                              <div style={{gap : "11px"}} className='flex'>
-                                <div className="icon-link"></div>
-                                <div className="icon-github"></div>
+                return(
+                  <motion.article 
+                        layout
+                        initial={{transform: "scale(0.4)"}}
+                        animate={{transform: "scale(1)"}}
+                        transition={{type: "spring" , damping: 8, stiffness: 50}}
+                        key={item.imgpath}  className='card'>
+                        <img width={266} src={item.imgpath} alt="" />
+                        <div style={{width:"266px"}} className="box">
+                              <h1 className="title">{item.projectTitle}</h1>
+                              <p className="sub-title">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, sed! Quidem, fugit quia maxime natus illum soluta quo voluptatibus laudantium consequuntur sapiente voluptates atque amet pariatur repudiandae quaerat, quod ipsa!
+                              </p>
+                              <div  className="flex icons">
+                                  <div style={{gap : "11px"}} className='flex'>
+                                    <div className="icon-link"></div>
+                                    <div className="icon-github"></div>
+                                  </div>
+                                  <a className='link flex' href="">
+                                    more 
+                                    <span style={{alignSelf: "end"}} className='icon-arrow-right'>
+
+                                    </span>
+                                  </a>
                               </div>
-                              <a className='link flex' href="">
-                                more 
-                                <span style={{alignSelf: "end"}} className='icon-arrow-right'>
+                        </div>
 
-                                </span>
-                              </a>
-                          </div>
-                    </div>
+                  </motion.article>
+                )
 
-              </article>
-            )
-
-          })}
-              
+              })}
+          </AnimatePresence>  
           </section>
     </main>
   )
